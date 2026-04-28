d10a290188ccc8e3357bab3da3c5181adec19eac
Dang Tran <dangtranmyha03092003@gmail.com>
2026-04-29 00:48:40 +0700

    add data for this example

--- Assignment3/ex3/ex3.c
+++ Assignment3/ex3/ex3.c
@@ -1,66 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LINES_PER_PAGE 60
-#define LINE_BUFFER 4096
-
-static void start_page(const char *filename, int page_no, int *is_first_output)
+void printFile(char *filename, int page_count)
 {
-    if (!*is_first_output)
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
     {
-        putchar('\f');
-        putchar('\n');
+        fprintf(stderr, "Khong the mo file: %s\n", filename);
+        return;
     }
 
-    printf("=== %s | Page %d ===\n\n", filename, page_no);
-    *is_first_output = 0;
-}
+    int c;
 
-static int print_file(FILE *fp, const char *filename, int *is_first_output)
-{
-    char line[LINE_BUFFER];
-    int page_no = 1;
-    int lines_on_page = 0;
+    printf("File: %s - Trang: %d\n\n", filename, page_count);
 
-    start_page(filename, page_no, is_first_output);
-
-    while (fgets(line, sizeof(line), fp) != NULL)
+    while ((c = fgetc(fp)) != EOF)
     {
-        fputs(line, stdout);
-        lines_on_page++;
-
-        if (lines_on_page >= LINES_PER_PAGE)
-        {
-            page_no++;
-            lines_on_page = 0;
-            start_page(filename, page_no, is_first_output);
-        }
+        putchar(c);
     }
 
-    return 0;
+    printf("\n\f");
+    fclose(fp);
 }
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        fprintf(stderr, "Usage: %s <file1> [file2 ...]\n", argv[0]);
+        printf("Su dung: %s <file1> <file2> ...\n", argv[0]);
         return 1;
     }
 
-    int is_first_output = 1;
-
     for (int i = 1; i < argc; i++)
     {
-        FILE *fp = fopen(argv[i], "r");
-        if (fp == NULL)
-        {
-            perror(argv[i]);
-            continue;
-        }
-
-        print_file(fp, argv[i], &is_first_output);
-        fclose(fp);
+        printFile(argv[i], i);
     }
 
     return 0;
