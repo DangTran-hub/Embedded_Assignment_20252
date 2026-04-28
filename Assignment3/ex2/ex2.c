6ea95aabce2a5b9ea621590d0bcee023f7d17d0e
Dang Tran <dangtranmyha03092003@gmail.com>
2026-04-29 00:26:50 +0700

    fix ex2 Assign 3

--- Assignment3/ex2/ex2.c
+++ Assignment3/ex2/ex2.c
@@ -1,61 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE 4096
-
-static int read_line(FILE *fp, char *buffer, size_t size)
+int main()
 {
-    return fgets(buffer, (int)size, fp) != NULL;
-}
+    FILE *f1, *f2;
+    char file1[100], file2[100], c1[1000], c2[1000];
+    int line = 1;
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
+    printf("Nhập tên tệp 1: ");
+    scanf("%s", file1);
+    printf("Nhập tên tệp 2: ");
+    scanf("%s", file2);
+
+    f1 = fopen(file1, "r");
+    f2 = fopen(file2, "r");
+
+    if (f1 == NULL || f2 == NULL)
     {
-        fprintf(stderr, "Usage: %s <file1> <file2>\n", argv[0]);
+        printf("Error.\n");
         return 1;
     }
 
-    FILE *f1 = fopen(argv[1], "r");
-    if (f1 == NULL)
+    while (fgets(c1, sizeof(c1), f1) != NULL && fgets(c2, sizeof(c2), f2) != NULL)
     {
-        perror(argv[1]);
-        return 1;
-    }
-
-    FILE *f2 = fopen(argv[2], "r");
-    if (f2 == NULL)
-    {
-        perror(argv[2]);
-        fclose(f1);
-        return 1;
-    }
-
-    char line1[MAX_LINE];
-    char line2[MAX_LINE];
-    long line_no = 1;
-
-    while (1)
-    {
-        int has1 = read_line(f1, line1, sizeof(line1));
-        int has2 = read_line(f2, line2, sizeof(line2));
-
-        if (!has1 && !has2)
+        if (strcmp(c1, c2) != 0)
         {
-            printf("Two files are identical.\n");
-            break;
+            printf("Khác biệt ở dòng %d:\n", line);
+            printf("Tệp 1: %s", c1);
+            printf("Tệp 2: %s", c2);
+            fclose(f1);
+            fclose(f2);
+            return 0;
         }
+        line++;
+    }
 
-        if (!has1 || !has2 || strcmp(line1, line2) != 0)
-        {
-            printf("First difference at line %ld:\n", line_no);
-            printf("%s: %s", argv[1], has1 ? line1 : "<EOF>\n");
-            printf("%s: %s", argv[2], has2 ? line2 : "<EOF>\n");
-            break;
-        }
-
-        line_no++;
+    if (fgets(c1, sizeof(c1), f1) != NULL || fgets(c2, sizeof(c2), f2) != NULL)
+    {
+        printf("Các tệp khác nhau về độ dài.\n");
+    }
+    else
+    {
+        printf("Hai tệp giống nhau.\n");
     }
 
     fclose(f1);
